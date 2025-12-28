c82bad28434def62a8667c2c73e892c2136f7970
Md. Tasnimul Hasan <114396737+mdtasnimulh@users.noreply.github.com>
2025-12-28 12:06:36 +0600

    working on letter screen

--- feature/home/src/main/java/com/tasnimulhasan/home/ui/HomeScreen.kt
+++ feature/home/src/main/java/com/tasnimulhasan/home/ui/HomeScreen.kt
@@ -11,7 +11,6 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -20,7 +19,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -185,7 +183,7 @@ internal fun HomeScreen(
 
         else -> {
             LazyColumn(
-                modifier = Modifier
+                modifier = modifier
                     .fillMaxSize()
                     .padding(horizontal = 16.dp)
             ) {
