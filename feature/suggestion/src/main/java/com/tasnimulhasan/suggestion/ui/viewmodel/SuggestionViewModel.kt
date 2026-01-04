3616631db7f7e1ced6b060cb9e5010a6d65d6a3c
Md. Tasnimul Hasan <114396737+mdtasnimulh@users.noreply.github.com>
2026-01-04 16:50:05 +0600

    suggestion module created with screen and navigation

---
+++ feature/suggestion/src/main/java/com/tasnimulhasan/suggestion/ui/viewmodel/SuggestionViewModel.kt
@@ -0,0 +1,11 @@
+package com.tasnimulhasan.suggestion.ui.viewmodel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SuggestionViewModel @Inject constructor(
+
+) {
+
+}
