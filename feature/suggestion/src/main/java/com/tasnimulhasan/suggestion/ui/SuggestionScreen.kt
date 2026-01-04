d7160321518f045b29a493ebef947ec1cb28de43
Md. Tasnimul Hasan <114396737+mdtasnimulh@users.noreply.github.com>
2026-01-04 16:52:13 +0600

    suggestion module created with screen and navigation

--- feature/suggestion/src/main/java/com/tasnimulhasan/suggestion/ui/SuggestionScreen.kt
+++ feature/suggestion/src/main/java/com/tasnimulhasan/suggestion/ui/SuggestionScreen.kt
@@ -1,10 +1,13 @@
 package com.tasnimulhasan.suggestion.ui
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.tasnimulhasan.suggestion.ui.viewmodel.SuggestionViewModel
 
 @Composable
 fun SuggestionScreen(
     navigateBack: () -> Unit,
+    viewModel: SuggestionViewModel = hiltViewModel()
 ) {
 
 }
