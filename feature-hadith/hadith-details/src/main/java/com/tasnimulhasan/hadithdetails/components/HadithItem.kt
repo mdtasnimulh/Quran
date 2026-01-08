1e487ed7efb7f83bdac8ddcc877503f9d2bcc871
Md. Tasnimul Hasan <114396737+mdtasnimulh@users.noreply.github.com>
2026-01-08 16:11:31 +0600

    pagination implemented for hadiths

---
+++ feature-hadith/hadith-details/src/main/java/com/tasnimulhasan/hadithdetails/components/HadithItem.kt
@@ -0,0 +1,117 @@
+package com.tasnimulhasan.hadithdetails.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tasnimulhasan.designsystem.theme.DullBlue
+import com.tasnimulhasan.designsystem.theme.RobotoFontFamily
+import com.tasnimulhasan.entity.hadith.HadithData
+
+@Composable
+fun HadithItem(
+    hadith: HadithData,
+){
+    var isExpanded by remember { mutableStateOf(false) }
+
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        elevation = CardDefaults.cardElevation(6.dp),
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            if (hadith.headingEnglish.isNotEmpty()) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    text = "(${hadith.hadithNumber}) Chapter: ${hadith.headingEnglish}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        fontFamily = RobotoFontFamily,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        textAlign = TextAlign.Start,
+                    ),
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                text = hadith.englishNarrator,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = RobotoFontFamily,
+                    color = DullBlue,
+                    textAlign = TextAlign.Start,
+                ),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                text = hadith.hadithEnglish,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = RobotoFontFamily,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Justify,
+                ),
+                maxLines = if (isExpanded) Int.MAX_VALUE else 3,
+                overflow = if (isExpanded) TextOverflow.Visible else TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded }
+                    .padding(horizontal = 6.dp, vertical = 2.dp),
+                text = if (isExpanded) "Show Less" else "View Full...",
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = RobotoFontFamily,
+                    color = DullBlue,
+                    textAlign = TextAlign.End
+                ),
+            )
+        }
+    }
+}
--- feature-hadith/hadith-details/src/main/java/com/tasnimulhasan/hadithdetails/ui/HadithDetailsScreen.kt
+++ feature-hadith/hadith-details/src/main/java/com/tasnimulhasan/hadithdetails/ui/HadithDetailsScreen.kt
@@ -7,13 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,15 +20,13 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.tasnimulhasan.designsystem.theme.RobotoFontFamily
 import com.tasnimulhasan.domain.apiusecase.hadith.FetchHadithsUseCase
-import com.tasnimulhasan.entity.hadith.HadithData
+import com.tasnimulhasan.hadithdetails.components.HadithItem
 import com.tasnimulhasan.hadithdetails.ui.viewmodel.HadithDetailsViewModel
 import com.tasnimulhasan.hadithdetails.ui.viewmodel.UiAction
 
@@ -97,6 +92,10 @@ internal fun HadithDetailsScreen(
                     .fillMaxSize()
                     .padding(horizontal = 16.dp)
             ) {
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+
                 items(uiState.hadiths) { hadith ->
                     HadithItem(hadith)
                     Spacer(modifier = Modifier.height(16.dp))
@@ -115,39 +114,6 @@ internal fun HadithDetailsScreen(
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun HadithItem(
-    hadith: HadithData,
-){
-    ElevatedCard(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        elevation = CardDefaults.cardElevation(6.dp),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                text = "(${hadith.hadithNumber}) ${hadith.hadithEnglish}",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Medium,
-                    fontFamily = RobotoFontFamily,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    textAlign = TextAlign.Justify,
-                ),
-            )
         }
     }
 }
